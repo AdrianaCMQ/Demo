e3785219100552c962543315ccb587b8d3c11a99
AdrianaCMQ <nicolechenmq@163.com>
2023-04-13 13:03:40 +0800

    [mengqi] feat: test for order service that saving order

---
+++ src/main/java/com/tw/oquizfinal/applications/OrderService.java
@@ -0,0 +1,13 @@
+package com.tw.oquizfinal.applications;
+
+import com.tw.oquizfinal.domain.order.Order;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class OrderService {
+    public Order save(Order order) {
+        return null;
+    }
+}
---
+++ src/test/java/com/tw/oquizfinal/applications/OrderServiceTest.java
@@ -0,0 +1,68 @@
+package com.tw.oquizfinal.applications;
+
+import com.tw.oquizfinal.domain.order.Order;
+import com.tw.oquizfinal.domain.order.OrderRepository;
+import com.tw.oquizfinal.domain.orderItem.OrderItem;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
+public class OrderServiceTest {
+
+    @Mock
+    private OrderRepository orderRepository;
+    @InjectMocks
+    private OrderService orderService;
+
+    public static final String TEST_ADDRESSEE = "test addressee";
+    public static final String TEST_ADDRESS = "test address";
+    public static final String MOBILE = "12345678987";
+    public static final int QUANTITY = 10;
+    public static final BigDecimal PRICE = BigDecimal.TEN;
+
+    private final OrderItem orderItem = new OrderItem();
+    private final Order order = new Order();
+
+    @BeforeEach
+    void Initialize() {
+        orderItem.setItemId(1L);
+        orderItem.setProductId(1L);
+        orderItem.setQuantity(QUANTITY);
+
+        order.setOrderId(1L);
+        order.setAddressee(TEST_ADDRESSEE);
+        order.setAddress(TEST_ADDRESS);
+        order.setMobile(MOBILE);
+        order.setTotalPrice(PRICE);
+        order.setCouponId(1L);
+        order.setItems(List.of(orderItem));
+    }
+
+    @Test
+    void should_return_order_when_save_it() {
+        when(orderRepository.save(order)).thenReturn(order);
+
+        Order savedOrder = orderService.save(order);
+
+        assertEquals(order.getOrderId(),savedOrder.getOrderId());
+        assertEquals(order.getAddressee(),savedOrder.getAddressee());
+        assertEquals(order.getAddress(),savedOrder.getAddress());
+        assertEquals(order.getMobile(),savedOrder.getMobile());
+        assertEquals(order.getTotalPrice(),savedOrder.getTotalPrice());
+        assertEquals(order.getCreatedAt(),savedOrder.getCreatedAt());
+        assertEquals(List.of(orderItem), savedOrder.getItems());
+    }
+}
