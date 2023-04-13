591361bfbe929aced44c878aa7393ba483f5dcef
AdrianaCMQ <nicolechenmq@163.com>
2023-04-13 13:05:15 +0800

    [mengqi] feat: save order in order service

--- src/main/java/com/tw/oquizfinal/applications/OrderService.java
+++ src/main/java/com/tw/oquizfinal/applications/OrderService.java
@@ -1,13 +1,16 @@
 package com.tw.oquizfinal.applications;
 
 import com.tw.oquizfinal.domain.order.Order;
+import com.tw.oquizfinal.domain.order.OrderRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class OrderService {
+    private final OrderRepository orderRepository;
+
     public Order save(Order order) {
-        return null;
+        return orderRepository.save(order);
     }
 }
