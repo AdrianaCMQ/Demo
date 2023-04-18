826bf937c86cda1359442199646540008d83fe4e
AdrianaCMQ <nicolechenmq@163.com>
2023-04-18 09:08:33 +0800

    [mengqi] refactor: change to parameter test

--- src/test/java/com/tw/oquizfinal/adapters/OrderControllerTest.java
+++ src/test/java/com/tw/oquizfinal/adapters/OrderControllerTest.java
@@ -8,7 +8,11 @@ import com.tw.oquizfinal.domain.order.Order;
 import com.tw.oquizfinal.domain.orderItem.OrderItem;
 import com.tw.oquizfinal.support.exceptions.ApiError;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -22,10 +26,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.math.BigDecimal;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -77,77 +78,77 @@ public class OrderControllerTest {
         orderItemWithProduct.setQuantity(QUANTITY);
     }
 
-    @Test
-    void should_return_success_when_save_order() throws Exception {
-        OrderRequest orderRequest = new OrderRequest(TEST_ADDRESSEE, TEST_ADDRESS, MOBILE, 1L, List.of(orderItem));
-        when(orderService.save(any())).thenReturn(order);
-        when(orderService.getOrderItemsWithProduct(List.of(orderItem))).thenReturn(List.of(orderItemWithProduct));
+    @Nested
+    class saveOrder {
+        @Test
+        void should_return_success_when_save_order() throws Exception {
+            OrderRequest orderRequest = new OrderRequest(TEST_ADDRESSEE, TEST_ADDRESS, MOBILE, 1L, List.of(orderItem));
+            when(orderService.save(any())).thenReturn(order);
+            when(orderService.getOrderItemsWithProduct(List.of(orderItem))).thenReturn(List.of(orderItemWithProduct));
 
-        mockMvc.perform(MockMvcRequestBuilders
-                        .post("/orders")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(new ObjectMapper().writeValueAsString(orderRequest))
-                )
-                .andExpect(status().isCreated());
-    }
-
-    @Test
-    void parameterisedShouldReturnBadRequestWhenRequestHaveEmptyField() throws Exception {
-        Map<List<OrderItem>, String> args = new HashMap<>();
-        args.put(Collections.emptyList(), "orderItems cannot be empty");
-        args.put(null, "orderItems cannot be empty");
-        for (Map.Entry<List<OrderItem>, String> entity : args.entrySet()) {
-            shouldReturnBadRequestWhenRequestHaveEmptyField(entity.getKey(), entity.getValue());
+            mockMvc.perform(MockMvcRequestBuilders
+                            .post("/orders")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(new ObjectMapper().writeValueAsString(orderRequest))
+                    )
+                    .andExpect(status().isCreated());
         }
-    }
 
-    void shouldReturnBadRequestWhenRequestHaveEmptyField(List<OrderItem> orderItems, String errors) throws Exception {
-        OrderRequest orderRequest = new OrderRequest("addressee", "address", "15771534215", 1L, orderItems);
+        @ParameterizedTest
+        @MethodSource("provideEmptyOrderItemsAndErrors")
+        void parameterised_should_return_bad_request_when_request_have_empty_field(List<OrderItem> orderItems, String errors) throws Exception {
+            OrderRequest orderRequest = new OrderRequest("addressee", "address", "15771534215", 1L, orderItems);
 
-        MockHttpServletResponse response = mockMvc.perform(MockMvcRequestBuilders
-                        .post("/orders")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(new ObjectMapper().writeValueAsString(orderRequest))
-                )
-                .andExpect(status().isBadRequest())
-                .andReturn()
-                .getResponse();
+            MockHttpServletResponse response = mockMvc.perform(MockMvcRequestBuilders
+                            .post("/orders")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(new ObjectMapper().writeValueAsString(orderRequest))
+                    )
+                    .andExpect(status().isBadRequest())
+                    .andReturn()
+                    .getResponse();
 
-        ApiError apiError = new ObjectMapper().readValue(response.getContentAsString(), ApiError.class);
-        List<String> responseMessage = Stream.of(apiError.getMessage().split(";")).collect(Collectors.toList());
+            ApiError apiError = new ObjectMapper().readValue(response.getContentAsString(), ApiError.class);
+            List<String> responseMessage = Stream.of(apiError.getMessage().split(";")).toList();
 
-        assertTrue(responseMessage.contains(errors));
-    }
-
-    @Test
-    void parameterisedShouldReturnBadRequestWhenRequestHaveBlankField() throws Exception {
-        Map<String, List<String>> args = new HashMap<>();
-        List<String> errorStringList = List.of("addressee cannot be blank", "address cannot be blank", "mobile cannot be blank");
-        args.put(null, errorStringList);
-        args.put("", errorStringList);
-        args.put("  ", errorStringList);
-
-        for (Map.Entry<String, List<String>> entity : args.entrySet()) {
-            shouldReturnBadRequestWhenRequestHaveBlankField(entity.getKey(), entity.getValue());
+            assertTrue(responseMessage.contains(errors));
         }
-    }
 
-    void shouldReturnBadRequestWhenRequestHaveBlankField(String blankField, List<String> errors) throws Exception {
-        OrderItem orderItem = new OrderItem(1L, 1L, 10);
-        OrderRequest orderRequest = new OrderRequest(blankField, blankField, blankField, 1L, List.of(orderItem));
+        static Stream<Arguments> provideEmptyOrderItemsAndErrors() {
+            return Stream.of(
+                    Arguments.of(Collections.emptyList(), "orderItems cannot be empty"),
+                    Arguments.of(null, "orderItems cannot be empty")
+            );
+        }
 
-        MockHttpServletResponse response = mockMvc.perform(MockMvcRequestBuilders
-                        .post("/orders")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(new ObjectMapper().writeValueAsString(orderRequest))
-                )
-                .andExpect(status().isBadRequest())
-                .andReturn()
-                .getResponse();
+        @ParameterizedTest
+        @MethodSource("provideBlankFieldsAndErrors")
+        void parameterised_should_return_bad_request_when_request_have_blank_field(String blankField, List<String> errors) throws Exception {
+            OrderItem orderItem = new OrderItem(1L, 1L, 10);
+            OrderRequest orderRequest = new OrderRequest(blankField, blankField, blankField, 1L, List.of(orderItem));
 
-        ApiError apiError = new ObjectMapper().readValue(response.getContentAsString(), ApiError.class);
-        List<String> responseMessage = Stream.of(apiError.getMessage().split(";")).collect(Collectors.toList());
+            MockHttpServletResponse response = mockMvc.perform(MockMvcRequestBuilders
+                            .post("/orders")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(new ObjectMapper().writeValueAsString(orderRequest))
+                    )
+                    .andExpect(status().isBadRequest())
+                    .andReturn()
+                    .getResponse();
 
-        errors.forEach(message -> assertTrue(responseMessage.contains(message)));
+            ApiError apiError = new ObjectMapper().readValue(response.getContentAsString(), ApiError.class);
+            List<String> responseMessage = Stream.of(apiError.getMessage().split(";")).toList();
+
+            errors.forEach(message -> assertTrue(responseMessage.contains(message)));
+        }
+
+        static Stream<Arguments> provideBlankFieldsAndErrors() {
+            List<String> errorStringList = List.of("addressee cannot be blank", "address cannot be blank", "mobile cannot be blank");
+            return Stream.of(
+                    Arguments.of(null, errorStringList),
+                    Arguments.of("", errorStringList),
+                    Arguments.of("  ", errorStringList)
+            );
+        }
     }
 }
